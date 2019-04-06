05247afcb34dc2d7181be2d85ca8de66404fe90a
James Simonson <james.simo@gmail.com>
2019-04-06 15:58:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Airflow Plugin Starter
+
+Bash script which take a single word input and scaffolds an Apache Airflow Plugin folder including `setup.py` file per [Airflow docs](https://airflow.apache.org/plugins.html#plugins-as-python-packages).
+
+The following example takes input `ibm`.
+
+```
+$ ./plugin_starter.sh ibm`
+```
+
+Creates plugin structure like below
+
+```
+├── __init__.py
+├── hooks
+│   ├── __init__.py
+│   └── ibm_hook.py
+├── operators
+│   ├── __init__.py
+│   └── ibm_starter_operator.py
+└── setup.py
+```
---
+++ plugin_starter.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+set -e
+
+function error() {
+    echo "Error: $1"
+    exit -1
+}
+
+PLUGIN_NAME=$1
+
+function execute {
+  
+    local plugin="${PLUGIN_NAME}_plugin"
+    local PluginName="$(tr '[:lower:]' '[:upper:]' <<< ${PLUGIN_NAME:0:1})${PLUGIN_NAME:1}"
+    if [ -d "$plugin" ]; then
+      error "Folder already exists: $plugin"
+    fi
+    echo "Creating new plugin: $plugin"
+    echo "Creating plugin and subfolders"
+    mkdir -p ${plugin}/hooks && echo "" > ${plugin}/hooks/__init__.py
+    mkdir -p ${plugin}/operators && echo "" > ${plugin}/operators/__init__.py
+    echo "from airflow.plugins_manager import AirflowPlugin
+
+from ${plugin}.hooks.${PLUGIN_NAME}_hook import ${PluginName}Hook
+from ${plugin}.operators.${PLUGIN_NAME}_starter_operator import ${PluginName}StarterOperator
+
+
+class ${PluginName}Plugin(AirflowPlugin):
+    name = \"${plugin}\"
+    operators = [${PluginName}StarterOperator]
+    hooks = [${PluginName}Hook]
+
+" > ${plugin}/__init__.py
+
+    # setup.py
+    echo "from setuptools import setup
+
+setup(
+    name=\"${PLUGIN_NAME}-plugin\",
+    entry_points = { 'airflow.plugins': ['${plugin} = ${PLUGIN_NAME}-plugin.${plugin}:${PluginName}Plugin'] }
+)
+
+" > ${plugin}/setup.py
+
+    # Hook
+    echo "from airflow.hooks.http_hook import HttpHook
+    
+class ${PluginName}Hook(HttpHook):
+  def __init__(self, ${PLUGIN_NAME}_conn_id=\"${PLUGIN_NAME}_default\"):
+      self.access_token = None
+      super().__init__(http_conn_id=${PLUGIN_NAME}_conn_id)
+
+  def get_conn(self, headers):
+      if self.access_token:
+          headers = {\"Authorization\": \"token {0}\".format(self.access_token)}
+          session = super().get_conn(headers)
+          session.auth = None
+          return session
+      return super().get_conn(headers)
+
+" > ${plugin}/hooks/${PLUGIN_NAME}_hook.py
+
+  # Operator
+  echo "import logging
+  
+from airflow.utils.decorators import apply_defaults
+from airflow.models import BaseOperator
+  
+from ${plugin}.hooks.${PLUGIN_NAME}_hook import ${PluginName}Hook
+
+class ${PluginName}StarterOperator(BaseOperator):
+    \"\"\"
+    ${PluginName} Starter Operator
+
+    Description about ${PluginName} Starter Operator
+
+    :param ${PLUGIN_NAME}_conn_id:              The ${PLUGIN_NAME} connection id.
+    :type ${PLUGIN_NAME}_conn_id:              string
+    :param required_param:              Required parameter description.
+    :type required_param:               string
+    :param optional_param:              *optional* Optional parameter description.
+    :type optional_param:               string
+    :param optional_payload:            *optional* Optional payload parameter description.
+    :type optional_payload:             dict
+    \"\"\"
+
+    template_fields = ('required_param',)
+    ui_color = '#133a78'
+    ui_fgcolor = '#ffffff'
+
+    @apply_defaults
+    def __init__(self,
+                  ${PLUGIN_NAME}_conn_id,
+                  required_param,
+                  optional_param=None,
+                  optional_payload={},
+                  **kwargs):
+        super().__init__(**kwargs)
+        self.${PLUGIN_NAME}_conn_id = ${PLUGIN_NAME}_conn_id
+        self.required_param = required_param
+        self.optional_param = optional_param
+        self.optional_payload = optional_payload
+
+    def execute(self, context):
+        self.${PLUGIN_NAME}_hook = ${PluginName}Hook(self.${PLUGIN_NAME}_conn_id)
+        output = []
+        # Do some work on output
+        # i.e. imagine ${PluginName}Hook contains method do_work() which takes input required_param
+        # output = ${PLUGIN_NAME}_hook.do_work(self.required_param)
+        return output
+  
+  " > ${plugin}/operators/${PLUGIN_NAME}_starter_operator.py
+
+  # Optional initialize plugin with virtualenv, activate and install linter, formatter and airflow
+  cd ${plugin} && virtualenv --python python3 env && source env/bin/activate && pip install pylint black apache-airflow
+  # Open plugin project with code editor
+  code ${plugin}
+}
+
+execute
